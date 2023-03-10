2fc4c24a2ee2b8e28647c36b00dcf37a79cb9ef7
Syilva <syilvariadi@student.telkomuniversity.ac.id>
2023-03-10 10:53:26 +0700

    commit v1.1

--- looping.py
+++ looping.py
@@ -22,3 +22,28 @@ irene_adler = {
 orang = [arsene_lupin, sherlock_holmes, irene_adler]
 
 print(orang)
+
+
+# membuat dictionary kosong
+orang = []
+
+# menerima input dari user dan menambahkan ke dalam dictionary
+for i in range(3):
+    nama = input(f"Masukkan nama orang ke-{i+1}: ")
+    nik = int(input(f"Masukkan NIK orang ke-{i+1}: "))
+    jenis_kelamin = input(f"Masukkan jenis kelamin orang ke-{i+1} (male/female): ")
+    tanggal_lahir = input(f"Masukkan tanggal lahir orang ke-{i+1} (format: YYYY-MM-DD): ")
+    
+    # membuat dictionary untuk setiap orang
+    data_orang = {
+        'nama': nama,
+        'nik': nik,
+        'jenis_kelamin': jenis_kelamin,
+        'tanggal_lahir': tanggal_lahir
+    }
+    
+    # menambahkan dictionary orang ke dalam list orang
+    orang.append(data_orang)
+
+# print hasil
+print(orang)
