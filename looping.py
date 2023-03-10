a457d0871986ebb9642ab04e2f26ce4ffd1901ee
Syilva <syilvariadi@student.telkomuniversity.ac.id>
2023-03-10 10:43:43 +0700

    commit v1.0

--- looping.py
+++ looping.py
@@ -1,11 +1,23 @@
-def is_even(number):
-    if number % 2 == 0:
-        return True
-    else:
-        return False
+# membuat dictionary kosong
+orang = []
 
-for i in range(1, 101):
-    if is_even(i) == False:
-        print(i)
-    else:
-        continue
+# menerima input dari user dan menambahkan ke dalam dictionary
+for i in range(3):
+    nama = input(f"Masukkan nama orang ke-{i+1}: ")
+    nik = int(input(f"Masukkan NIK orang ke-{i+1}: "))
+    jenis_kelamin = input(f"Masukkan jenis kelamin orang ke-{i+1} (male/female): ")
+    tanggal_lahir = input(f"Masukkan tanggal lahir orang ke-{i+1} (format: YYYY-MM-DD): ")
+    
+    # membuat dictionary untuk setiap orang
+    data_orang = {
+        'nama': nama,
+        'nik': nik,
+        'jenis_kelamin': jenis_kelamin,
+        'tanggal_lahir': tanggal_lahir
+    }
+    
+    # menambahkan dictionary orang ke dalam list orang
+    orang.append(data_orang)
+
+# print hasil
+print(orang)
